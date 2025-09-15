da5a2b5f7f8004ca5b0d46f44625722b7baf103b
Bailey Williams (1000139616) <WILLBJ6@student.op.ac.nz>
2025-09-16 10:49:50 +1200

    Top section done

--- Program.cs
+++ Program.cs
@@ -5,18 +5,17 @@
         static void Main(string[] args)
         {
             // Top //
-
-
-
-
-
-
-
-
-
-
-
-
+            Console.WriteLine("This month surly couldn't have gotten any worse,");
+            Thread.Sleep(750);
+            Console.WriteLine("Started today at rock bottom....");
+            Thread.Sleep(500);
+            Console.WriteLine("now im deeper.");
+            Thread.Sleep(500);
+            Console.WriteLine("lets hope tomorrow is better");
+            Thread.Sleep(500);
+            Console.WriteLine("but I have a exam tomrrow");
+            Thread.Sleep(500);
+            Console.WriteLine("and tomorrows monday");
             // Middle //
 
 
@@ -39,7 +38,7 @@
 
 
 
-            
+
         }
     }
 }
