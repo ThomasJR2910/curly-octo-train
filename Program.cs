31265984b61f891e6c2784954254294e5c8a4186
Thomas Roberts (1000113228) <ROBETJ4@student.op.ac.nz>
2025-09-16 10:47:53 +1200

    Added middle section

--- Program.cs
+++ Program.cs
@@ -18,19 +18,19 @@
 
 
             // Middle //
-
-
-
-
-
-
-
-
-
-
-
-
-
+            Console.ReadLine()
+            Console.WriteLine("Indeed tomorrow was Monday")
+            Console.WriteLine("But Jimmy was a little to tired to realise that it was monday")
+            Console.WriteLine("So Jimmy went down to the zoo")
+            Console.WriteLine("Not realising that it was monday")
+            Console.WriteLine("The zoo is closed on monday")
+            Console.WriteLine("The animals roam free on a monday")
+            Console.WriteLine("Jimmy opened the gates and slipped inside")
+            Console.WriteLine("It was quiet")
+            Console.WriteLine("Almost too quiet")
+            Console.WriteLine("But Jimmy didn't care")
+            Console.WriteLine("He wanted an ice cream from the ice cream cart")
+            Console.WriteLine("And then there was a bear!")
             // Bottom //
 
 
