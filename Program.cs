fe317388720d621179fca7f047fb7eba417f4f51
Bailey Williams (1000139616) <WILLBJ6@student.op.ac.nz>
2025-09-16 10:54:37 +1200

    fixed Thomas' code

--- Program.cs
+++ Program.cs
@@ -19,19 +19,19 @@ namespace ConsoleApp1
             Thread.Sleep(500);
             Console.WriteLine("and tomorrows monday");
             // Middle //
-            Console.ReadLine()
-            Console.WriteLine("Indeed tomorrow was Monday")
-            Console.WriteLine("But Jimmy was a little to tired to realise that it was monday")
-            Console.WriteLine("So Jimmy went down to the zoo")
-            Console.WriteLine("Not realising that it was monday")
-            Console.WriteLine("The zoo is closed on monday")
-            Console.WriteLine("The animals roam free on a monday")
-            Console.WriteLine("Jimmy opened the gates and slipped inside")
-            Console.WriteLine("It was quiet")
-            Console.WriteLine("Almost too quiet")
-            Console.WriteLine("But Jimmy didn't care")
-            Console.WriteLine("He wanted an ice cream from the ice cream cart")
-            Console.WriteLine("And then there was a bear!")
+            Console.ReadLine();
+            Console.WriteLine("Indeed tomorrow was Monday");
+            Console.WriteLine("But Jimmy was a little to tired to realise that it was monday");
+            Console.WriteLine("So Jimmy went down to the zoo");
+            Console.WriteLine("Not realising that it was monday");
+            Console.WriteLine("The zoo is closed on monday");
+            Console.WriteLine("The animals roam free on a monday");
+            Console.WriteLine("Jimmy opened the gates and slipped inside");
+            Console.WriteLine("It was quiet");
+            Console.WriteLine("Almost too quiet");
+            Console.WriteLine("But Jimmy didn't care");
+            Console.WriteLine("He wanted an ice cream from the ice cream cart");
+            Console.WriteLine("And then there was a bear!");
             // Bottom //
             Console.ReadLine();
             Console.WriteLine("The bear (not from the TV show 'The bear') sold me a new guitar.");
